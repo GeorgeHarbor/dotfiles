bfbb7dfae2ea63ad636c30880210cc7957692ed4
Luka Djordjevic <ldjordjevic@protonmail.com>
2023-04-19 19:47:27 +0200

    Init

---
+++ nvim/init.lua
@@ -0,0 +1,37 @@
+for _, source in ipairs {
+  "astronvim.bootstrap",
+  "astronvim.options",
+  "astronvim.lazy",
+  "astronvim.autocmds",
+  "astronvim.mappings",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
+
+if astronvim.default_colorscheme then
+  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+    require("astronvim.utils").notify("Error setting up colorscheme: " .. astronvim.default_colorscheme, "error")
+  end
+end
+require("hop").setup()
+require("prettier").setup {
+  bin = "prettier", -- or `'prettierd'` (v0.23.3+)
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+}
+-- require("omnisharp-vim").setup()
+
+require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
