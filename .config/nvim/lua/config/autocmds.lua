391f5e3f800d7804b86f03489bb5974fee4ebcc1
= <=>
2025-05-26 12:13:44 +0200

    Updates, easydotnet filetype assertion and clipboard autocmd

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,47 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  once = true,
+  callback = function()
+    if vim.fn.has("win32") == 1 or vim.fn.has("wsl") == 1 then
+      vim.g.clipboard = {
+        copy = {
+          ["+"] = "clip.exe",
+          ["*"] = "clip.exe",
+        },
+        paste = {
+          ["+"] = "powershell.exe -c Get-Clipboard",
+          ["*"] = "powershell.exe -c Get-Clipboard",
+        },
+      }
+    elseif vim.fn.has("unix") == 1 then
+      if vim.fn.executable("xclip") == 1 then
+        vim.g.clipboard = {
+          copy = {
+            ["+"] = "xclip -selection clipboard",
+            ["*"] = "xclip -selection clipboard",
+          },
+          paste = {
+            ["+"] = "xclip -selection clipboard -o",
+            ["*"] = "xclip -selection clipboard -o",
+          },
+        }
+      elseif vim.fn.executable("wl-copy") == 1 and vim.fn.executable("wl-paste") then
+        vim.g.clipboard = {
+          copy = {
+            ["+"] = "wl-copy",
+            ["*"] = "wl-copy",
+          },
+          paste = {
+            ["+"] = "wl-paste",
+            ["*"] = "wl-paste",
+          },
+        }
+      end
+    end
+
+    vim.opt.clipboard = "unnamedplus"
+  end,
+  desc = "Lazy load clipboard",
+})
--- .config/nvim/lua/plugins/easy-dotnet.lua
+++ .config/nvim/lua/plugins/easy-dotnet.lua
@@ -1,6 +1,7 @@
 return {
   "GustavEikaas/easy-dotnet.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+  ft = { "cs", "csx", "csproj", "sln" },
   config = function()
     require("easy-dotnet").setup()
   end,
